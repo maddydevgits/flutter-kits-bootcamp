bd7657fa9c0ac2078bf7f9fda0ad64ddd379e239
MS <72651078+maddydevgits@users.noreply.github.com>
2025-07-07 21:09:34 +0530

    Create second_page.dart

---
+++ experiment5/second_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Back to Home'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
