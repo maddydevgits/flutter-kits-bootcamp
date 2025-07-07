8373071edf24eebc3d188ff566d079c738f2fd64
MS <72651078+maddydevgits@users.noreply.github.com>
2025-07-07 21:09:10 +0530

    Create home_page.dart

---
+++ experiment5/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'second_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Second Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
